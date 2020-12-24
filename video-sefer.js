78d5081ff98ffd8f5d706380414e148c0ed60198
bz <szvbinjomin@gmail.com>
2020-12-24 22:16:00 +0100

    chapter inc/dec counter on left/right click

--- video-sefer.js
+++ video-sefer.js
@@ -1,13 +1,28 @@
-window.onload = function() {
-  let link = document.createElement("link");
-  link.href = "chumas.css";
-  link.rel = "stylesheet"; 
-  link.type = "text/css";
-  frames['chumas'].document.head.appendChild(link);
+$(document).ready(function () {
+	$("#chapter").click(function(event){
+		var now = $(this).html() * 1;
+		$(this).html(now + 1);
+	});
 
-  let link2 = document.createElement("link");
-  link2.href = "rasi.css";
-  link2.rel = "stylesheet"; 
-  link2.type = "text/css"; 
-  frames['rasi'].document.head.appendChild(link2);
+	$("#chapter").contextmenu(function(event){
+		var now = $(this).html() * 1;
+		$(this).html(now - 1);
+		return false;
+	});
+});
+
+window.onload = function() {
+	let link = document.createElement("link");
+	link.href = "chumas.css";
+	link.rel = "stylesheet"; 
+	link.type = "text/css";
+	frames['chumas'].document.head.appendChild(link);
+
+	let link2 = document.createElement("link");
+	link2.href = "rasi.css";
+	link2.rel = "stylesheet"; 
+	link2.type = "text/css"; 
+	frames['rasi'].document.head.appendChild(link2);
+
 }
+
